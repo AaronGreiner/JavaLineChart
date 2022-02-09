8e55f1ef8de7c4c46ba981dda6570df0cf1ee4cf
Aaron Greiner <aarongreiner@icloud.com>
2022-02-09 07:22:29 +0100

    Init

---
+++ LineChartProject/src/linechartproject/LineChartProject.java
@@ -0,0 +1,11 @@
+package linechartproject;
+
+public class LineChartProject {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Test");
+        
+    }
+    
+}
