b6200c790dc8512b2338ea61de22db8ae8e718f6
Aaron Greiner <aarongreiner@icloud.com>
2022-02-10 17:56:10 +0100

    Umstellung auf Einfache Java Klasse

--- LineChartProject/src/components/LineChartPanel.java
+++ LineChartProject/src/components/LineChartPanel.java
@@ -1,4 +1,4 @@
-package linechartproject;
+package components;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -7,15 +7,22 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
+import javax.swing.JPanel;
 
-public class LineChartPanel extends javax.swing.JPanel {
-
+public class LineChartPanel extends JPanel {
+    
+    private boolean debug_mode = false;
+    private boolean check_highlight_close_point = false;
+    
     private int offset_border = 20;
-    private Font font = new Font("Segoe UI", Font.PLAIN, 12);
     
     private Point x_pos = new Point();
     private Point y_pos = new Point();
@@ -33,23 +40,46 @@ public class LineChartPanel extends javax.swing.JPanel {
     
     public LineChartPanel() {
         
-        //Daten Zufällig generieren:
-        for (int i = 0; i < x_numElements; i++) {
-            x_listString.add(LocalDate.now().plusDays(i).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-            x_listValue.add((int)(Math.random()*1000));
-        }
+        loadDebugData();
         
-        initComponents();
+        init();
     }
-
+    
+    private void init() {
+        
+        setMaximumSize(new Dimension(200, 100));
+        setBorder(javax.swing.BorderFactory.createLineBorder(new Color(0, 0, 0)));
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent evt) {
+                chartMouseMoved(evt);
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent evt) {
+                chartMouseEntered(evt);
+            }
+            @Override
+            public void mouseExited(MouseEvent evt) {
+                chartMouseExited(evt);
+            }
+        });
+    }
+    
     protected void paintComponent(Graphics g) {
         
-        super.paintComponent(g); //Zurücksetzen vom vorherigen Paint
+        //Zurücksetzen vom vorherigen Paint:
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //Antialiasing
+        
+        //Wenn keine Elemente gefüllt, dann nichts malen:
+        if (x_listValue.size() <= 0) {
+            return;
+        }
         
         x_listPoints.clear();
-        
-        Graphics2D g2 = (Graphics2D) g; //wird für Font-Antialiasing benötigt
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //Antialiasing
         
         //Positionen berechnen:
         Dimension d = this.getSize();
@@ -74,8 +104,8 @@ public class LineChartPanel extends javax.swing.JPanel {
         //Linien anzeigen:
         g2.setColor(Color.white);
         
-        g2.drawLine(zero_pos.x, zero_pos.y, x_pos.x, x_pos.y);
-        g2.drawLine(zero_pos.x, zero_pos.y, y_pos.x, y_pos.y);
+        g2.drawLine(zero_pos.x, zero_pos.y, x_pos.x, x_pos.y); //X Achse
+        g2.drawLine(zero_pos.x, zero_pos.y, y_pos.x, y_pos.y); //Y Achse
         
         //X-Achse Einteilung:
         int x_length = x_pos.x - zero_pos.x;
@@ -128,13 +158,22 @@ public class LineChartPanel extends javax.swing.JPanel {
         }
         
         //Test MausMovement;
-        if ( mouse_pos != null ) {
+        if ( check_highlight_close_point && mouse_pos != null ) {
             Point point_temp = new Point(getClosestPoint());
             if (mouse_dist < 50) {
                 g2.setColor(Color.red);
                 g2.fillOval(point_temp.x - 6, point_temp.y - 6, 12, 12);
             }
         }        
+    }
+    
+    private void loadDebugData() {
+        
+        //Daten Zufällig generieren:
+        for (int i = 0; i < x_numElements; i++) {
+            x_listString.add(LocalDate.now().plusDays(i).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+            x_listValue.add((int)(Math.random()*1000));
+        }
     }
     
     private Point getClosestPoint(){
@@ -166,29 +205,30 @@ public class LineChartPanel extends javax.swing.JPanel {
         return ret;
     }
     
-
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseMoved(java.awt.event.MouseEvent evt) {
-                formMouseMoved(evt);
-            }
-        });
-        setLayout(null);
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void formMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseMoved
+    public void setDebugMode(boolean debug_mode) {
+        this.debug_mode = debug_mode;
+    }
+    
+    private void chartMouseMoved(MouseEvent evt) {
         mouse_pos.x = evt.getX();
         mouse_pos.y = evt.getY();
 //        System.out.println("X:" + mouse_pos.x + " Y:" + mouse_pos.y);
         
         this.repaint();
-    }//GEN-LAST:event_formMouseMoved
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
+    }
+    
+    private void chartMouseEntered(MouseEvent evt) {
+        
+        check_highlight_close_point = true;
+        
+        this.repaint();
+    }
+    
+    private void chartMouseExited(MouseEvent evt) {
+        
+        check_highlight_close_point = false;
+        
+        this.repaint();
+    }
+    
 }
--- LineChartProject/src/linechartproject/Frame.java
+++ LineChartProject/src/linechartproject/Frame.java
@@ -12,7 +12,7 @@ public class Frame extends javax.swing.JFrame {
 
         btnBack = new javax.swing.JButton();
         btnForward = new javax.swing.JButton();
-        lineChartPanel1 = new linechartproject.LineChartPanel();
+        lineChartPanel1 = new components.LineChartPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Test - LineChart");
@@ -21,6 +21,17 @@ public class Frame extends javax.swing.JFrame {
         btnBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/arrow_back.png"))); // NOI18N
 
         btnForward.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/arrow_forward.png"))); // NOI18N
+
+        javax.swing.GroupLayout lineChartPanel1Layout = new javax.swing.GroupLayout(lineChartPanel1);
+        lineChartPanel1.setLayout(lineChartPanel1Layout);
+        lineChartPanel1Layout.setHorizontalGroup(
+            lineChartPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        lineChartPanel1Layout.setVerticalGroup(
+            lineChartPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 648, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -32,7 +43,7 @@ public class Frame extends javax.swing.JFrame {
                     .addComponent(lineChartPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnBack)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 928, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 966, Short.MAX_VALUE)
                         .addComponent(btnForward)))
                 .addContainerGap())
         );
@@ -40,7 +51,7 @@ public class Frame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(lineChartPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 650, Short.MAX_VALUE)
+                .addComponent(lineChartPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(btnForward)
@@ -54,6 +65,6 @@ public class Frame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBack;
     private javax.swing.JButton btnForward;
-    private linechartproject.LineChartPanel lineChartPanel1;
+    private components.LineChartPanel lineChartPanel1;
     // End of variables declaration//GEN-END:variables
 }
